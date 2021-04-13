04947194a83d2953040e7c951cf07dec5fd21e84
St3ve89 <istvan.acs89@gmail.com>
2021-04-13 14:33:07 +0100

    Adding A Few More Options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -6,7 +6,16 @@ const CodeEditor = () => {
       language="javascript"
       theme="dark"
       height="500px"
-      options={{ wordWrap: 'on', minimap: { enabled: false } }}
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
     />
   );
 };
