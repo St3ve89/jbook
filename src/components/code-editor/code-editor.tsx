24acd2c3f44c812f289368ae41b18b408010b974
St3ve89 <istvan.acs89@gmail.com>
2021-04-15 12:32:26 +0100

    Delete last line what prettier puts on format

--- src/components/code-editor/code-editor.tsx
+++ src/components/code-editor/code-editor.tsx
@@ -22,13 +22,15 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
 
   const onFormatClick = () => {
     const unforamtted = editorRef.current.getModel().getValue();
-    const formatted = prettier.format(unforamtted, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formatted = prettier
+      .format(unforamtted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
 
     editorRef.current.setValue(formatted);
   };
