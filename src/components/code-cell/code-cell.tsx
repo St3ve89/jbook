8a259cfce52b2803c74000a107459053671dce4d
St3ve89 <istvan.acs89@gmail.com>
2021-04-21 11:04:19 +0100

    Capturing Bundling Errors

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -6,12 +6,14 @@ import Resizable from '../resizable/resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [error, setError] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setError(output.error);
     }, 1000);
 
     return () => {
