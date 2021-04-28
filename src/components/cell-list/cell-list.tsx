8658191850e6451e7b0ab79a1ee482cd36de2aea
St3ve89 <istvan.acs89@gmail.com>
2021-04-28 09:42:55 +0100

    Creating a Typed Selector

--- src/components/cell-list/cell-list.tsx
+++ src/components/cell-list/cell-list.tsx
@@ -1,4 +1,10 @@
+import { useTypedSelector } from '../../hooks/use-typed-selector';
+
 const CellList: React.FC = () => {
+  const cellState = useTypedSelector((state) => state);
+
+  console.log({ cellState });
+
   return <div>Cell List</div>;
 };
 
