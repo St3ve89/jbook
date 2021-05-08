936f5a701fdd8cbf68c6d6884030fe86f5ae4b88
St3ve89 <istvan.acs89@gmail.com>
2021-05-08 15:40:37 +0100

    Adding the Action Creator

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,11 +1,15 @@
+import { Dispatch } from 'redux';
+import bundle from '../../bundler';
 import { ActionType } from '../action-types';
 import {
-  Action,
   DeleteCellAction,
   Direction,
   InsertCellAfterAction,
   MoveCellAction,
   UpdateCellAction,
+  BundleStartAction,
+  BundleCompleteAction,
+  Action,
 } from '../actions';
 import { CellTypes } from '../cell';
 
@@ -43,5 +47,26 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      },
+    });
   };
 };
